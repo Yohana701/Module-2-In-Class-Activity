5e0663054476303de89c81dff97d13a23d5cd9ac
Yohana701 <105068132+Yohana701@users.noreply.github.com>
2024-07-06 14:51:33 -0700

    Create QuartsToGallonsInteractive.java

---
+++ QuartsToGallonsInteractive.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class QuartsToGallonsInteractive{
+    public static void main(String[] args){
+        final int QUARTS_IN_GALLONS = 4;
+        int quartsNeeded;
+        int gallonsNeeded;
+        int extraQuartsNeeded;
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Enter quarts needed >>");
+            quartsNeeded = input.nextInt();
+        }
+        gallonsNeeded = quartsNeeded/ QUARTS_IN_GALLONS;
+        extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLONS;
+        System.out.println("A job that needs " + quartsNeeded + " quarts requires " + gallonsNeeded + " gallons plus " + extraQuartsNeeded + " quarts. ");
+    }
+}
